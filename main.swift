b452eef9fbdec366257d3c90b19ace133920af19
20161104614 <964829629@qq.com>
2018-09-12 11:25:50 +0800

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  leijia
+//
+//  Created by 彭皓 on 2018/9/12.
+//  Copyright © 2018年 彭皓. All rights reserved.
+//
+
+import Foundation
+var sum = 0
+for index in 1...100{
+    sum = sum + index
+}
+print(sum)
+
